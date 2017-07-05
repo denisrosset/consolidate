7f421f6c9ec31b9bc928b04ec044adb3805d551b
Denis Rosset <physics@denisrosset.com>
2017-07-05 17:01:41 +0200

    Added ListSet instance.

--- src/main/scala/com.faacets/consolidate/instances/set.scala
+++ src/main/scala/com.faacets/consolidate/instances/set.scala
@@ -3,6 +3,8 @@ package instances
 
 import cats.data.NonEmptyList
 
+import scala.collection.immutable.ListSet
+
 trait SetInstances {
 
   implicit def consolidateMergeForSet[A] = new Merge[Set[A]] {
@@ -17,6 +19,14 @@ trait SetInstances {
 
   }
 
-
+  implicit def consolidateMergeForListSet[A] = new Merge[ListSet[A]] {
+    def merge(base: ListSet[A], other: ListSet[A]): Result[ListSet[A]] =  {
+      val newElements = other -- base
+      if (newElements.isEmpty)
+        Result.same(base)
+      else
+        Result.updated(base ++ newElements, NonEmptyList.of(Path.empty -> s"new elements = $newElements"))
+    }
+  }
 
 }
